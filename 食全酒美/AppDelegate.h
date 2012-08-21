34f64636728b4090f613c410668568c7fa6f188e
gongzhenliang <devdev@devmatoiMac.local>
2012-08-21 10:31:25 +0800

    Initial Commit

---
+++ 食全酒美/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  食全酒美
+//
+//  Created by dev dev on 12-8-21.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ 食全酒美/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  食全酒美
+//
+//  Created by dev dev on 12-8-21.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
